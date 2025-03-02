4b6d2bdf71cf3c5b4c953eb67a9350b49eb901ba
Rubén Tostón Carrero <ruben.toston@edu.uah.es>
2025-03-02 16:24:14 +0100

    Plane controlled by joystick and mouse (using PyGame)

--- lab04-camel/lab04-camel.py
+++ lab04-camel/lab04-camel.py
@@ -1,4 +1,5 @@
 import arcade
+
 from arcade import MOUSE_BUTTON_LEFT
 from arcade import MOUSE_BUTTON_RIGHT
 from random import *
@@ -61,22 +62,21 @@ class MyGame(arcade.Window):
         out = Out_line(700, 400, 200, 100, arcade.color.BLACK, 5)
         self.outline.append(out)
         #jugador
-        self.eleccion_jug = 0
-        self.posicion_jug = 0
+        self.eleccion_jug = None
+        self.posicion_jug = None
         self.gol_jug = 0
         self.turno_jug=True
         # bot
-        self.eleccion_IA = 0
-        self.posicion_IA = 0
+        self.eleccion_IA = None
+        self.posicion_IA = None
         self.gol_IA = 0
         self.turno_IA=False
         # ronda y numero límite de rondas
         self.ronda = 1
         self.final = 5
         self.dibujar_rectangulo=True
-        self.mensaje=""
-
-
+        self.mensaje="Choose your shot"
+        self.portada=True
 
 
 
@@ -95,10 +95,47 @@ class MyGame(arcade.Window):
                          arcade.color.WHITE, 20)
 
         arcade.draw_text(self.mensaje, 400, 500, arcade.color.CYAN, 20)
-        if self.turno_jug:
-            self.mensaje="Choose your shot"
-        elif self.turno_IA:
-            self.mensaje="Choose your move"
+
+
+
+
+    def bot_action_keeper(self,delta_time):
+        self.mensaje="Choose your shot"
+
+        self.eleccion_IA = 0
+        self.posicion_IA = 0
+        print(f"Turno de IA: Parar...")
+
+        self.eleccion_IA = randint(1, 3)
+
+        if self.eleccion_IA == 1 or self.eleccion_IA == 3:
+            self.posicion_IA = randint(1, 2)
+        else:
+            self.posicion_IA=None
+
+        self.turno_IA=True
+        arcade.unschedule(self.bot_action_keeper)
+        arcade.schedule(self.comprobar_acciones_tiro, 2)
+        arcade.schedule(self.bot_action_shoot, 4)
+
+    def bot_action_shoot(self,delta_time):
+        self.mensaje="Choose your move"
+        self.eleccion_IA = 0
+        self.posicion_IA = 0
+        print(f"Turno de IA: Tirar...")
+
+        self.eleccion_IA = randint(1, 3)
+
+        if self.eleccion_IA == 1 or self.eleccion_IA == 3:
+            self.posicion_IA = randint(1, 2)
+        else:
+            self.posicion_IA = None
+        arcade.unschedule(self.bot_action_shoot)
+        self.turno_jug=True
+        self.dibujar_rectangulo=True
+        self.turno_IA=False
+
+
 
 
 
@@ -106,9 +143,6 @@ class MyGame(arcade.Window):
     def on_mouse_press(self, x, y, button, modifiers):
 
         if self.turno_jug:
-            self.eleccion_jug = 0
-            self.posicion_jug = 0
-
             #Definir los límites del área de detección (rectángulo en este caso)
             rect_x1,rect_y1,rect_width1,rect_height1= 500,350,200,200
             rect_x2, rect_y2, rect_width2, rect_height2 = 700, 300, 200, 100
@@ -121,55 +155,137 @@ class MyGame(arcade.Window):
                (rect_y1 - rect_height1 / 2) <= y <= (rect_y1 + rect_height1 / 2) :
                 self.eleccion_jug=2
                 self.posicion_jug=None
-                self.dibujar_rectangulo=False
+                self.dibujar_rectangulo = False
+                self.turno_jug = False
+
+
             elif (rect_x2 - rect_width2 / 2) <= x <= (rect_x2 + rect_width2 / 2) and \
                  (rect_y2 - rect_height2 / 2) <= y <= (rect_y2 + rect_height2 / 2) :
                 self.eleccion_jug = 3
                 self.posicion_jug=2
                 self.dibujar_rectangulo = False
+                self.turno_jug = False
+
+
             elif (rect_x3 - rect_width3 / 2) <= x <= (rect_x3 + rect_width3 / 2) and \
                  (rect_y3 - rect_height3 / 2) <= y <= (rect_y3 + rect_height3 / 2) :
                 self.eleccion_jug = 1
                 self.posicion_jug=2
                 self.dibujar_rectangulo = False
-            elif (rect_x3 - rect_width3 / 2) <= x <= (rect_x3 + rect_width3 / 2) and \
-                 (rect_y3 - rect_height3 / 2) <= y <= (rect_y3 + rect_height3 / 2) :
-                self.eleccion_jug = 1
-                self.posicion_jug=2
-                self.dibujar_rectangulo = False
+                self.turno_jug = False
+
+
             elif (rect_x4 - rect_width4 / 2) <= x <= (rect_x4 + rect_width4 / 2) and \
                  (rect_y4 - rect_height4 / 2) <= y <= (rect_y4 + rect_height4 / 2) :
                 self.eleccion_jug = 1
                 self.posicion_jug=1
                 self.dibujar_rectangulo = False
+                self.turno_jug = False
+
+
             elif (rect_x5 - rect_width5 / 2) <= x <= (rect_x5 + rect_width5 / 2) and \
                  (rect_y5 - rect_height5 / 2) <= y <= (rect_y5 + rect_height5 / 2):
                 self.eleccion_jug = 3
                 self.posicion_jug = 1
                 self.dibujar_rectangulo = False
-            print(self.eleccion_jug,self.posicion_jug)
+                self.turno_jug=False
+
+#comprobaciones
+
+        elif not self.turno_jug:
+            return
+
+        if not self.turno_IA:
+            arcade.schedule(self.bot_action_keeper,1)
 
 
 
 
-    def bot_action(self,delta_time):
+        print(self.eleccion_jug,self.posicion_jug)
 
 
 
-        self.eleccion_IA = 0
-        self.posicion_IA = 0
+    def comprobar_acciones_tiro(self,delta_time):
+        """Para comprobar tiros del jugador"""
+        # no coincide:gol
+        if self.eleccion_jug != self.eleccion_IA:
 
-        self.turno_jug=True
+            print("Gol")
+            self.gol_jug += 1
 
-        arcade.schedule(self.mensaje,3)
+        # coincide:comprobar
+        # primer caso:izquierda tira jugador
+        elif self.eleccion_jug == 1 and self.eleccion_IA == 1:
+
+            if self.posicion_jug == self.posicion_IA:
+
+                print("Parada")
+
+            else:
+
+                print("Gol")
+                self.gol_jug += 1
+
+        # segundo caso:centro tira jugador
+        elif self.eleccion_jug == 2 and self.eleccion_IA == 2:
+
+            print("Parada");
+
+        # tercer caso:derecha tira jugador
+        elif self.eleccion_jug == 3 and self.eleccion_IA == 3:
+
+            if self.posicion_jug == self.posicion_IA:
+
+                print("Parada")
+
+            else:
+
+                print("Gol")
+                self.gol_jug += 1
+
+        arcade.unschedule(self.comprobar_acciones_tiro)
+
+    def comprobar_acciones_parada(self,delta_time):
+        """Para comprobar paradas del jugador"""
+        # no coincide:gol
+        if self.eleccion_jug != self.eleccion_IA:
+
+            print("Gol")
+            self.gol_IA += 1
 
 
+        # coincide:comprobar
+        # primer caso:izquierda tira jugador
+        elif self.eleccion_jug == 1 and self.eleccion_IA == 1:
 
-        self.eleccion_IA = randint(1, 3)
+            if self.posicion_jug == self.posicion_IA:
 
-        if self.eleccion_IA == 1 or self.eleccion_IA == 3:
-            self.posicion_IA = randint(1, 2)
+                print("Parada")
 
+            else:
+
+                print("Gol")
+                self.gol_IA += 1
+
+
+        # segundo caso:centro tira jugador
+        elif self.eleccion_jug == 2 and self.eleccion_IA == 2:
+
+            print("Parada");
+
+        # tercer caso:derecha tira jugador
+        elif self.eleccion_jug == 3 and self.eleccion_IA == 3:
+
+            if self.posicion_jug == self.posicion_IA:
+
+                print("Parada")
+
+            else:
+
+                print("Gol")
+                self.gol_jug += 1
+
+        arcade.unschedule(self.comprobar_acciones_tiro)
 
 
 
